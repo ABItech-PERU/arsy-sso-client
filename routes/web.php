b799df6a8ac6663755840d0eed90d24c3b307d10
abitech-dev <abimaelfv@hotmail.com>
2026-06-19 23:10:07 -0500

    fix(routes): actualizar nombres de metodos login, callback y logout

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use Arsy\SSOClient\Http\Controllers\SsoLoginController;
 use Arsy\SSOClient\Http\Controllers\SsoWebhookController;
 
 Route::middleware(['web'])->group(function () {
-    Route::get('/login', [SsoLoginController::class, 'getLogin'])->name('login');
-    Route::get('/auth/callback', [SsoLoginController::class, 'getCallback']);
-    Route::post('/logout', [SsoLoginController::class, 'getLogout'])->name('logout');
+    Route::get('/login', [SsoLoginController::class, 'login'])->name('login');
+    Route::get('/auth/callback', [SsoLoginController::class, 'callback']);
+    Route::post('/logout', [SsoLoginController::class, 'logout'])->name('logout');
 });
 
 Route::middleware(['api'])->prefix('api')->group(function () {
